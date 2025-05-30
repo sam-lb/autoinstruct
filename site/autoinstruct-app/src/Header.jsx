0ae5a911eea6503e10819ffaca327f783559c577
sam-lb <samlbrunacini@gmail.com>
2025-05-30 17:01:34 -0400

    added a NavItem component for the nav links

--- site/autoinstruct-app/src/Header.jsx
+++ site/autoinstruct-app/src/Header.jsx
@@ -1,13 +1,15 @@
 
+import NavItem from "./NavItem";
+
 
 function Header() {
     return (
         <header>
             <h1>Autoinstruct: instant instructional videos.</h1>
-            <nav>
-                <ul>Home</ul>
-                <ul>About</ul>
-                <ul>Examples</ul>
+            <nav id="header-nav">
+                <NavItem text="Home"/>
+                <NavItem text="About"/>
+                <NavItem text="Examples"/>
             </nav>
         </header>
     );
---
+++ site/autoinstruct-app/src/NavItem.jsx
@@ -0,0 +1,11 @@
+
+
+function NavItem({ text }) {
+    return (
+        <div class="nav-item">
+            <a href="#">{ text }</a>
+        </div>
+    );
+}
+
+export default NavItem
