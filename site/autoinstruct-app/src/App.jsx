73826f7ca572e3f70e65c1383c1bc4d15e9dcc86
sam-lb <samlbrunacini@gmail.com>
2025-05-30 17:15:33 -0400

    added a Footer component containing copyright

--- site/autoinstruct-app/src/App.jsx
+++ site/autoinstruct-app/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
 
 function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   );
 }
---
+++ site/autoinstruct-app/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+    return (
+        <footer>
+            <div id="footer-wrapper">
+                <p>&copy; {(new Date()).getFullYear()} Sam Brunacini</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer
