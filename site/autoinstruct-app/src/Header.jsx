e92562a2e49ed054957943a1cdf679b50ddd4486
sam-lb <samlbrunacini@gmail.com>
2025-05-30 16:33:19 -0400

    removed boilerplate and made a header component

---
+++ site/autoinstruct-app/src/Header.jsx
@@ -0,0 +1,16 @@
+
+
+function Header() {
+    return (
+        <header>
+            <h1>Autoinstruct: instant instructional videos.</h1>
+            <nav>
+                <ul>Home</ul>
+                <ul>About</ul>
+                <ul>Examples</ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
